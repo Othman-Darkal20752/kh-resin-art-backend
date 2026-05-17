12e4da61721257ccba5d4689f61ab54d6542db3f
Othman-Darkal20752 <odsoftware4@gmail.com>
2026-05-17 12:21:26 +0300

    add health check endpoint

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,17 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import path, include
+
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("health/", health_check, name="health_check"),
     path("api/", include("catalog.urls")),
 ]
 
